451ad1b2969973a738f29e53a5e24841d6a1b218
Michal Brzozowski <miki@extremic.com>
2021-11-23 10:49:40 +0100

    Fix windows build, update gitignore

--- windows_build.sh
+++ windows_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-../zenon/zenon src/main.znn -o infinihack.exe --cpp="~/Downloads/mxe/usr/bin/i686-w64-mingw32.shared.dw2-g++ -O3 -Isrc/ -I/home/michal/Downloads/mxe/usr/x86_64-w64-mingw32.shared/include/SDL2" -lSDL2 -lSDL2_ttf
+../zenon/zenon src/main.znn -o infinihack.exe --cpp="~/Downloads/mxe/usr/bin/i686-w64-mingw32.shared.dw2-g++ -O3 -Isrc/ -I/home/michal/Downloads/mxe/usr/x86_64-w64-mingw32.shared/include/SDL2" -lmingw32 -l SDL2main -lSDL2 -lSDL2_ttf
 
