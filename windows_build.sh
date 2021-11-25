1320c17345b89cfa44c91cc5c5f14ba12d8bb5f8
Michal Brzozowski <miki@extremic.com>
2021-11-25 12:02:34 +0100

    Add packaging to the windows build script

--- windows_build.sh
+++ windows_build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-../zenon/zenon src/main.znn -o infinihack.exe --cpp="~/Downloads/mxe/usr/bin/i686-w64-mingw32.shared.dw2-g++ -O3 -Isrc/ -I/home/michal/Downloads/mxe/usr/x86_64-w64-mingw32.shared/include/SDL2" -lmingw32 -l SDL2main -lSDL2 -lSDL2_ttf
-
+DIR="brokenrogue-$1"
+mkdir $DIR
+../zenon/zenon src/main.znn -o $DIR/brokenrogue.exe --cpp="~/Downloads/mxe/usr/bin/i686-w64-mingw32.shared.dw2-g++ -O3 -Isrc/ -I/home/michal/Downloads/mxe/usr/x86_64-w64-mingw32.shared/include/SDL2" -lmingw32 -l SDL2main -lSDL2 -lSDL2_ttf
+cp *.dll $DIR
+cp *.txt $DIR
+cp *.ttf $DIR
+zip -r ../$DIR.zip $DIR
+rm -Rf $DIR
