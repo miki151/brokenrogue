144868c36d3355a248c21d39923a135ba491d9ab
Michal Brzozowski <miki@extremic.com>
2022-05-09 21:08:00 +0200

    Fix library paths, fix compile errors

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-../zenon/zenon src/main.znn -o main --cpp="clang++ -g -I/usr/include/SDL2 -Isrc/" -lSDL2 -lSDL2_ttf -I /home/michal/zenon_sdl -I /home/michal/zenon_ui -I /home/michal/zenon_pretty_archive
+../zenon/zenon src/main.znn -o main --cpp="clang++ -g -I/usr/include/SDL2 -Isrc/" -lSDL2 -lSDL2_ttf -lSDL2_image -I /home/michal/zenon_sdl -I /home/michal/zenon_ui -I /home/michal/zenon_pretty_archive
 
