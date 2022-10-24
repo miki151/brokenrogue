74030e08a1ec1ab0f346be43e4a5f8e5fb9dd8fa
Michal Brzozowski <miki@extremic.com>
2022-10-24 12:02:26 +0200

    Remove build script, add zenon_flags file

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-../zenon/zenon src/main.znn -o main --cpp="clang++ -g -I/usr/include/SDL2 -Isrc/" -lSDL2 -lSDL2_ttf -lSDL2_image -I /home/michal/zenon_sdl -I /home/michal/zenon_ui -I /home/michal/zenon_pretty_archive
-
